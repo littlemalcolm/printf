8d2a47d166b12e0d792efd24960b7ae635e5e884
littlemalcolm <babajideyusuf11@gmail.com>
2023-07-02 09:01:39 +0000

    choose.c

---
+++ choose.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  * choose_f - chooses the right modifier.
+  * @c: ...
+  * @args: ...
+  *
+  * Return: n
+  */
+int choose_f(char c, va_list args)
+{
+	int n = -1;
+
+	if (c == 'd' || c == 'i')
+		n = print_int(args);
+	else if (c == '%')
+		n = print_percent(1);
+	else if (c == 'c')
+		n = print_char(args);
+	else if (c == 's')
+		n = print_str(args);
+	else if (c == 'r')
+		n = print_rstr(args);
+	else if (c == 'R')
+		n = print_rot13_str(args);
+	else if (c == 'b')
+		n = print_binary(args);
+	else if (c == 'u')
+		n = print_unsigned(args);
+	else if (c == 'o')
+		n = print_octal(args);
+	else if (c == 'x')
+		n = print_hex(args);
+	else if (c == 'X')
+		n = print_HEX(args);
+	else if (c == 'S')
+		n = print_string(args);
+	else if (c == 'p')
+		n = print_address(args);
+	else
+		n = print_unknown(c);
+	return (n);
+}
